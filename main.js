0656bee6c631179ce2de0af2c71663620c49a24a
Walid Bilem <wldblm@icloud.com>
2021-09-16 21:30:47 +0200

    js finish

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
 import './style.css'
 
+const increase = document.getElementById('increase');
+const dcrease = document.getElementById('dcrease');
+const reset = document.getElementById('reset');
+const counter = document.getElementById('counter');
+
+let count = 0;
+
+increase.addEventListener('click', () => {
+  count ++;
+  counter.innerHTML = count;
+})
+dcrease.addEventListener('click', () => {
+  count --;
+  counter.innerHTML = count;
+})
+reset.addEventListener('click', () => {
+  count = 0;
+  counter.innerHTML = count;
+})
+
